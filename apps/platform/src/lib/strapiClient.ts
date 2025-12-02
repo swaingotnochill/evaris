c9cce657e6e6e967769041ea171700d21da1aedb
Roshan Swain <swainroshan001@gmail.com>
2025-12-02 07:45:04 +0100

    remove apps from here, will use a separate repo

--- apps/platform/src/lib/strapiClient.ts
+++
@@ -1,7 +0,0 @@
-import { strapi } from '@strapi/client'
-
-export const strapiClient = strapi({
-  baseURL: import.meta.env.VITE_STRAPI_URL,
-})
-
-export const articles = strapiClient.collection('articles')
